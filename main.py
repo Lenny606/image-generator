dff62018e345d4f94eac83df92556a1432d92d69
tomas.kravcik <thomas.kravcik@gmail.com>
2026-04-26 16:34:43 +0200

    feat: integrate SDXL Turbo pipeline to generate and save dog images

--- main.py
+++ main.py
@@ -1,6 +1,23 @@
+from diffusers import AutoPipelineForText2Image
+import torch
+
 def main():
     print("Welcome to Image Generator!")
     print("This is a boilerplate entry point for your Python application.")
+
+    model="stabilityai/sdxl-turbo"
+    pipeline = AutoPipelineForText2Image.from_pretrained(model).to('cpu')
+
+    height=512
+    width=512
+    steps=1
+
+    prompt="a photo of dog, black and white, looking at camera"
+
+    image= pipeline(prompt=prompt, height=height, width=width, num_inference_steps=steps, guidance_scale=0.0).images[0]
+
+    image.save("dog.png")
+    
 
 if __name__ == "__main__":
     main()
